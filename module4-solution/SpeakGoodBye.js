759a74d965ad7b89375c192c766358a973d1efe4
Ankit Kumar Singh <chhotu12ank@gmail.com>
2020-05-17 01:42:09 +0530

    One edit made to mod4-sol

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -21,7 +21,6 @@
 	// STEP 8: Rewrite the 'speak' function such that it is attached to the
 	// byeSpeaker object instead of being a standalone function.
 	// See Lecture 52, part 2
-	byeSpeaker.speak();
 
 	// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 	// 'byeSpeaker' on the global scope as well.
